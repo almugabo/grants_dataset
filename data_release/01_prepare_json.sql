37d62576f5767da220d7cb7ab01c80fc38e5e490
MikeM <almugabo@gmail.com>
2023-11-14 22:33:16 +0100

    update

--- data_release/01_prepare_json.sql
+++ data_release/01_prepare_json.sql
@@ -9,69 +9,81 @@ here we first create the json data
 
 
 
--- first create index on tables which will be used 
+-- index on tables used 
+create index idx_tmp_fpfunding_projects      on ofundres_eufp_projects (project_id);
+create index idx_tmp_fpfunding_projects_prog on ofundres_eufp_programmes (project_id);
+create index idx_tmp_fpfunding_projects_org  on ofundres_eufp_organisations (project_id);
+create index idx_tmp_fpfunding_projects_res  on ofundres_eufp_researchers (project_id);
 
 
 
-create index idx_tmp_fpfunding_projects      on xx_projects (project_id);
-create index idx_tmp_fpfunding_projects_prog on xx_projects_programmes (project_id);
-create index idx_tmp_fpfunding_projects_org  on xx_projects_organisations (project_id);
-create index idx_tmp_fpfunding_projects_res  on xx_projects_researchers (project_id);
--- create index idx_tmp_fpfunding_projects_res_role   on xx_projects_researchers (person_role );
 
 
-drop table if exists zzzz_funding_data_opensearch_fp; 
 
-create table zzzz_funding_data_opensearch_fp
+drop table if exists ofundres_eufp_json; 
+
+create table ofundres_eufp_json
 as 
 SELECT 
 t1.project_id
 , json_object(
-'project_id', t1.project_id,
-'project_doi', t1.project_doi,
-'project_title', project_title,
-'project_title_short', project_title_short,
-'project_time_start', project_time_start,
-'project_time_end', project_time_end,
-'project_budget_ec_contribution', project_budget_ec_contribution,
-'project_content_abstract', project_content_abstract,
-'project_status', project_status,
-'programme', (select json_group_array (json_object ('prog_fp', t2.prog_fp,
-                                                    'programme_legal_basis', t2.programme_legal_basis,
-                                                    'programme_nr', t2.programme_nr,
-                                                    'programme_code', t2.programme_code,
-                                                    'programme_label', t2.programme_label
+'project_id'        ,t1.project_id,
+'project_url'        ,t1.project_url,
+'project_title'      ,t1.project_title,
+'project_title_short',t1.project_title_short,
+'project_time_start' ,t1.project_time_start,
+'project_time_end'   ,t1.project_time_end,
+'project_budget'     ,t1.project_budget,
+'project_budget_currency',t1.project_budget_currency,
+'project_description'    ,t1.project_description,
+'project_status'         ,t1.project_status,
+'project_fundorg'        ,t1.project_fundorg,
+'project_fundorg_name'  ,t1.project_fundorg_name,
+'programme', (select json_group_array (json_object ('programme_lv1_code'  ,t2.programme_lv1_code,
+                                                    'programme_lv1_label' ,t2.programme_lv1_label,
+                                                    'programme_lv2_code'  ,t2.programme_lv2_code,
+                                                    'programme_lv2_label' ,t2.programme_lv2_label,
+                                                    'programme_scheme'    ,t2.programme_scheme
                                                     )
                                        )
-                                       from xx_projects_programmes as t2
+                                       from ofundres_eufp_programmes as t2
                                        where t1.project_id = t2.project_id
+                                       
               ),
-'organisation', (select json_group_array (json_object ('org_role',  t3.org_role, 
-                                                       'org_name', t3.org_name, 
-                                                       'org_cuontry', t3.org_country
-                                                       )
-                                         )
-                                         FROM xx_projects_organisations as t3
-                                         where t1.project_id = t3.project_id
-              ), 
-'researcher', (select json_group_array (json_object ('person_role', t4.person_role, 
-                                                     'person_name_first', t4.person_name_first, 
-                                                     'person_name_last', t4.person_name_last
-                                                       )
-                                         )
-                                         FROM xx_projects_researchers as t4
-                                         where t1.project_id = t4.project_id
-                                         and t4.person_role = 'principalInvestigator'
-              )                                                        
-                                                    
+'organisation', (select json_group_array (json_object ('org_id'     ,t3.org_id,
+                                                       'org_name'   ,t3.org_name,
+                                                       'org_country',t3.org_country,
+                                                       'org_role'   ,t3.org_role
+                                                    )
+                                       )
+                                       from ofundres_eufp_organisations as t3
+                                       where t1.project_id = t3.project_id
+                                       
+              ) ,
+'researcher', (select json_group_array (json_object ('person_id'        ,t4.person_id,
+                                                     'person_role'      ,t4.person_role,
+                                                     'person_name_first',t4.person_name_first,
+                                                     'person_name_last' ,t4.person_name_last
+                                                    )
+                                       )
+                                       from ofundres_eufp_researchers as t4
+                                       where t1.project_id = t4.project_id
+                                       
+              )               
+              
+
 ) as json_data  
-FROM xx_projects as t1
---limit 2 
+FROM 
+ofundres_eufp_projects as t1 
 ;
 
---Execute time (ms)	43095
---Start time	2023-10-22 09:38:58.954
---Finish time	2023-10-22 09:39:42.081
+--Start time	2023-11-14 22:31:26.755
+--Finish time	2023-11-14 22:31:28.626
+
+
+
+
+
 
 
 
